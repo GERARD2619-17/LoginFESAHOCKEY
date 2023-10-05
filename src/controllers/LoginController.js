22f35abb804642d8cd734cea89d4cb913febaf9c
Arucha27 <aruchaeduardo@gmail.com>
2023-10-05 13:49:49 -0600

    dia 5 de octubre josue

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,12 +1,69 @@
-function login(req, res){
-res.render('login/index');
-}
-
-function register(req, res){
+function index(req, res) {
+    if (req.session.loggedin) {
+          // Output username
+      res.redirect('/');
+      
+    } else {
+      res.render('login/index');
+    }
+  }
+  
+  function register(req, res) {
     res.render('login/register');
-    }
-    module.exports ={
-        login:login,
-        register: register,
+  }
+
+  function auth(req, res) {
+    const data = req.body;   
+
+    req.getConnection((err, conn) => {
+      conn.query('SELECT * FROM users WHERE email = ?', [data.email], (err, userdata) =>{
+      if(userdata.length > 0) {
+
+
+          res.render('login/register', { error: 'Error: user alredy exists !' })
         
-    }
+     
+      } else{
+      }
+
+});
+});
+ }
+
+  function storeUser (req, res) {
+    const data = req.body;
+
+    req.getConnection((err, conn) => {
+      conn.query('SELECT * FROM users WHERE email = ?', [data.email], (err, userdata) =>{
+      if(userdata.length > 0) {
+
+
+          res.render('login/register', { error: 'Error: user alredy exists !' })
+        
+     
+      } else
+      bcrypt.hash(data.password, 12).then(hash => {
+        data. password = hash;
+    
+        req.getConnection ((err, conn) => {
+        conn.query('INSERT INTO users SET ?', [data], (err, rows) => {
+        res.redirect('/');
+        });
+        });
+        });
+      
+      
+      {
+
+        }
+      });
+      });
+       
+    }  
+  module.exports = {
+    index: index,
+    register: register,
+    storeUser:storeUser,
+   auth:auth,   
+  }
+  
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,10 +1,16 @@
-const express = require ('express');
-const LoginController = require ('../controllers/LoginController');
+const express = require('express');
+const LoginController = require('../controllers/LoginController');
 
-const router = express.Router ();  
+const router = express.Router();
+
+router.get('/login', LoginController.index);
+router.get('/register', LoginController.register);
+router.post('/register', LoginController.storeUser);
+router.post('/register', LoginController.auth);
 
 
-router.get ('/login', LoginController.login);
-router.get ('/register', LoginController.register);
+
+
+
 
 module.exports = router;
